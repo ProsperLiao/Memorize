59a026fc252d0ecea0e1b6c8cdb27c212e27bf05
Hongxing Liao <hx.liao@gmail.com>
2021-06-17 15:30:57 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Hongxing Liao on 2021/6/17.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
