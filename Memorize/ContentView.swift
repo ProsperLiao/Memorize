580a677cf7ac7a6ddab8e208f1236c76f6d79d67
Hongxing Liao <hx.liao@gmail.com>
2021-06-17 19:25:31 +0800

    feat: lesson 1

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            RoundedRectangle(cornerRadius: 20.0)
+                .stroke(lineWidth: 3)
+            
+            Text("Hello World!")
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
