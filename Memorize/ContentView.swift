7e8c7fea400b6a570a9a5d2338cf5e9db1aed4c9
Hongxing Liao <hx.liao@gmail.com>
2021-09-22 01:43:50 +0800

    feat: add 3 theme cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -7,26 +7,47 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
-    let emojis = ["🚗", "🚕", "🚙", "🚓", "🚌", "🚎", "🚑", "🏎", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🚲", "🛵", "🏍", "🛺", "🚨", "🚔", "🚍", "🚘", "🚖", "✈️", "🛫", "🛬", "🛥", "🛳", "⛴"]
+    let vehichles = ["🚗", "🚕", "🚙", "🚓", "🚌", "🚎", "🚑", "🏎", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🚲", "🛵", "🏍", "🛺", "🚨", "🚔", "🚍", "🚘", "🚖", "✈️", "🛫", "🛬", "🛥", "🛳", "⛴"]
+    let animals = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼", "🐻‍❄️", "🐨", "🐯", "🦁", "🐮", "🐷", "🐽", "🐸", "🐵", "🙈", "🙉", "🙊", "🐒", "🐔", "🐧", "🐦", "🐤", "🐣", "🐥"]
+    let foods = ["🍏", "🍎", "🍐", "🍊", "🍋", "🍌", "🍉", "🍇", "🍓", "🫐", "🍈", "🍒", "🍑", "🥭", "🍍", "🥥", "🥝", "🍅", "🍆", "🥑", "🥦", "🥬", "🥒", "🌶", "🫑", "🌽", "🥕"]
+    @State var emojis: [String] = ["🚗", "🚕", "🚙", "🚓", "🚌", "🚎", "🚑", "🏎", "🚒", "🚐", "🛻", "🚚", "🚛", "🚜", "🚲", "🛵", "🏍", "🛺", "🚨", "🚔", "🚍", "🚘", "🚖", "✈️", "🛫", "🛬", "🛥", "🛳", "⛴"]
+    
     @State var emojiCount = 6
     
     var body: some View {
         VStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                        CardView(content: emoji)
-                            .aspectRatio(2/3, contentMode: .fit)
-                    }
-                }
-                .foregroundColor(.red)
-            }
-            Spacer()
+            Text("Memorize!").font(.largeTitle)
             HStack {
                 remove
                 Spacer()
                 add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
+            Spacer()
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: widthThatBestFits(cardCount: emojiCount)))]) {
+                    if emojiCount > 0 {
+                        ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                            CardView(content: emoji)
+                                .aspectRatio(2/3, contentMode: .fit)
+                        }
+                    }
+                }
+                .foregroundColor(.red)
+            }
+            
+            Spacer()
+            HStack {
+                Spacer()
+                vehichle
+                Spacer()
+                animal
+                Spacer()
+                food
+                Spacer()
             }
             .font(.largeTitle)
             .padding(.horizontal)
@@ -54,7 +75,51 @@ struct ContentView: View {
             Image(systemName: "plus.circle")
         }
     }
+    
+    var vehichle: some View {
+        Button {
+            emojis = vehichles.shuffled()
+            emojiCount = Int.random(in: 4...emojis.count)
+        } label: {
+            VStack {
+                Image(systemName: "car")
+                Text("Vehichle").font(.body)
+            }
+            
+        }
+    }
+    
+    var animal: some View {
+        Button {
+            emojis = animals.shuffled()
+            emojiCount = Int.random(in: 4...emojis.count)
+        } label: {
+            VStack {
+                Image(systemName: "pawprint.circle")
+                Text("Animal").font(.body)
+            }
+        }
+    }
+
+    var food: some View {
+        Button {
+            emojis = foods.shuffled()
+            emojiCount = Int.random(in: 4...emojis.count)
+        } label: {
+            VStack {
+                Image(systemName: "heart.circle")
+                Text("Food").font(.body)
+            }
+        }
+    }
+    
+    func widthThatBestFits(cardCount: Int) -> CGFloat {
+        return 65
+    }
+    
 }
+
+
 
 
 
@@ -68,6 +133,8 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
         ContentView()
             .preferredColorScheme(.dark)
-            
+        ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 12 Pro Max"))
+            .previewDisplayName("12 Pro Max")
     }
 }
